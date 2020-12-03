dd754a53f81d58efe78bee9cdb2c3865aa583404
pedro <pedro_cepeda99@hotmail.com>
2020-12-04 00:23:16 +0100

    punto 0

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
 import networkx as nx
 
 class AStar:
-    print ("hola")
-    print ("santiago_lamela")
-
-
-    print("Pedro")
-
-
-
-
-    print("yago eres un cerdo")
-    print ("gonsas9elpelotudo")
-
-    print("Jorge")
-    print ("santiago_lamela")
