90b74906cdd8ed419c57534193f14b2161f00153
JorgeFernandez00 <jfernandezconde2000@gmail.com>
2020-12-03 23:05:43 +0100

    Añadidas clases contenidas en grafo y main

---
+++ GraphData/graph_data.py
@@ -0,0 +1,11 @@
+class Vertex:
+    def __init__(self, name, coordinates: list):
+        self.name = name
+        self.coordinates = coordinates
+
+    def aerial_distance(self, destination_coords: list):
+        #TODO: Implementar cálculos
+
+class Edge:
+    def __init__(self, estimated_time):
+        self.estimated_time = estimated_time
